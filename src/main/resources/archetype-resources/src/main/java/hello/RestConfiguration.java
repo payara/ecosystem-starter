d863ca927de9a21e7d5a0531f7614b3fefe8b777
Gaurav Gupta <gaurav.gupta@payara.fish>
2023-09-06 10:06:52 +0530

    FISH-7537 / FISH-7538 Add Payara project template to Payara Starter

---
+++ src/main/resources/archetype-resources/src/main/java/hello/RestConfiguration.java
@@ -0,0 +1,12 @@
+package ${package}.hello;
+
+import ${eePackage}.ws.rs.ApplicationPath;
+import ${eePackage}.ws.rs.core.Application;
+
+/**
+ * Configures RESTful Web Services for the application.
+ */
+@ApplicationPath("resources")
+public class RestConfiguration extends Application {
+    
+}
