aa520c5f7a6a77dd85b7d1f9f47084b9dda4dbd7
Gaurav Gupta <gaurav.gupta.jc@gmail.com>
2026-05-01 19:58:22 +0530

    FISH-9870  Use kebab case Docker image name to comply with Docker naming rules

--- starter-ui/src/main/java/fish/payara/starter/resources/ApplicationConfiguration.java
+++ starter-ui/src/main/java/fish/payara/starter/resources/ApplicationConfiguration.java
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2023-2024 Payara Foundation and/or its affiliates. All rights reserved.
+ * Copyright (c) 2023-2026 Payara Foundation and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -193,7 +193,7 @@ public class ApplicationConfiguration {
     }
 
     public String getArtifactId() {
-        return artifactId;
+        return StringUtil.toKebabCase(artifactId);
     }
 
     public void setArtifactId(String artifactId) {
